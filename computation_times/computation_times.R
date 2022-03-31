53e0fc73a5beabbe8c2599440d7032012263414b
j.klug <83557410+klugjani@users.noreply.github.com>
2022-03-31 11:48:05 +0200

    create folder computation times

---
+++ computation_times/computation_times.R
@@ -0,0 +1,66 @@
+###############################################################################
+# Landscape Ecology and Data Science, HU Berlin
+# Authors: Shawn Schneidereit, Janis Klug
+# Script for: Comparison of computational times of the rapidminer model runs
+# Date: 25.03.2022
+
+###############################################################################
+
+# Loading required libraries
+library(ggplot2)
+
+# Models: Naive Bayes, Deep Learning, Generalized linear model, Random Forest 
+# model type
+model_type <- c('NB', 'DL', 'GLM', 'RF')
+
+# Runtimes (ms)
+runtime <- c(295.332, 344.315, 304.432, 2426.231)
+
+# Training times (ms)
+training_time <- c(64, 595, 1000, 119)
+
+# Scoring times (ms)
+scoring_time <- c(2000, 1000, 574, 4000)
+
+###############################################################################
+
+# Creating a computation times dataframe
+df <- data.frame(model_type, total_time, runtime, 
+                 training_time, scoring_time)
+
+###############################################################################
+
+# Creating bar plots
+# Runtimes
+p_rt <-ggplot(data=df, aes(x=model_type, y=runtime)) +
+  geom_bar(stat="identity", width = 0.6, fill = c("#000000","#F0E442",
+                                                  "#009E73","#999999"))+
+  xlab("model type")+
+  ylab("runtimes (ms)")+
+  theme_classic()
+p_rt
+
+# Creating bar plots
+# training time
+p_trt <-ggplot(data=df, aes(x=model_type, y=training_time)) +
+  geom_bar(stat="identity", width = 0.6, fill = c("#000000","#F0E442",
+                                                  "#009E73","#999999"))+
+  xlab("model type")+
+  ylab("training time (ms)")+
+  theme_classic()
+p_trt
+
+
+# Creating bar plots
+# scoring time 
+p_st <-ggplot(data=df, aes(x=model_type, y=scoring_time)) +
+  geom_bar(stat="identity", width = 0.6, fill = c("#000000","#F0E442",
+                                                  "#009E73","#999999"))+
+  xlab("model type")+
+  ylab("scoring time (ms)")+
+  theme_classic()
+p_st
+
+
+
+
